2124ca979916342f536f40dc2aa7861588804dec
German-Valencia <lenovopromotor@gmail.com>
2022-02-08 18:59:09 -0500

    2 revisión

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import noImage from '../../img/noImage.png';
-import styles from './Card.module.css'
+import noImage from "../../img/noImage.png";
+import styles from "./Card.module.css";
 
-export default function Card({name, image, types, id}) {
-    
-    // console.log(name, image, types)
-    return(
+export default function Card({ name, image, types, id }) {
+  
+  return (
+    <div>
+      <NavLink className={styles.none} to={`/pokemon/${id}`}>
         <div>
-            <NavLink className={styles.none} to={`/pokemon/${id}`}>
-                <div>
-                    <img className={styles.img} src={image ? image : noImage} alt="img not found" width="200px" height="250vh" />
-                    <h2>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
-                    <div className={styles.types}>
-                        {
-                            types?.map((e, k) => {
-                                return (
-                                    <div className={styles.types} key={k}>
-                                        <img className={styles.typesImg} src={e.img} alt='X' />
-                                        <p className={styles.text}>{e.name.charAt(0).toUpperCase() + e.name.slice(1)}</p>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
+          <img
+            className={styles.img}
+            src={image ? image : noImage}
+            alt="img not found"
+            width="200px"
+            height="250vh"
+          />
+          <h2>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
+          <div className={styles.types}>
+            {types?.map((e, k) => {
+              return (
+                <div className={styles.types} key={k}>
+                  <img className={styles.typesImg} src={e.img} alt="X" />
+                  <p className={styles.text}>
+                    {e.name.charAt(0).toUpperCase() + e.name.slice(1)}
+                  </p>
                 </div>
-            </NavLink>
+              );
+            })}
+          </div>
         </div>
-    );
-};
+      </NavLink>
+    </div>
+  );
+}
